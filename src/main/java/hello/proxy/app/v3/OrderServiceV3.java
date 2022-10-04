8cc64b6c0d44cc527574fa4e0eee7570a82d380e
Yongje Shin <yongje1211@gmail.com>
2022-10-04 23:23:26 +0900

    수정

--- src/main/java/hello/proxy/app/v3/OrderServiceV3.java
+++ src/main/java/hello/proxy/app/v3/OrderServiceV3.java
@@ -1,14 +1,13 @@
 package hello.proxy.app.v3;
 
-import hello.proxy.app.v2.OrderRepositoryV2;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceV3 {
 
-    private final OrderRepositoryV2 orderRepository;
+    private final OrderRepositoryV3 orderRepository;
 
-    public OrderServiceV3(OrderRepositoryV2 orderRepository) {
+    public OrderServiceV3(OrderRepositoryV3 orderRepository) {
         this.orderRepository = orderRepository;
     }
 
